0360c07143f4c87c0f68f1b1e990e168feea4133
raed <raed@eng.leadshook.com>
2023-07-26 01:10:13 +0100

    add font cdn

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          />
         </Head>
         <body dir="rtl">
           <Main />
