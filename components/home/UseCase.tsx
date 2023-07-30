8ab1850f769c27eed427a5155a1ccb2e82228e51
raed <raed@eng.leadshook.com>
2023-07-30 03:08:29 +0100

    menufolio sections

--- components/home/UseCase.tsx
+++ components/home/UseCase.tsx
@@ -1,6 +1,26 @@
-import { Grid, Typography } from "@material-ui/core";
-
+import { Grid, Typography, Button } from "@material-ui/core";
+import { Divider } from "antd";
+import { map } from "lodash";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+interface UseCaseInterface {
+  image: string;
+  title: string;
+  description: string;
+  button: {
+    title: string;
+    link: string;
+  };
+}
 const UseCase = () => {
+  const [UseCases, setUseCases] = useState<UseCaseInterface[]>([]);
+  const getUseCases = async () => {
+    const _use = await import("./data/useCase.json");
+    setUseCases(_use);
+  };
+  useEffect(() => {
+    getUseCases();
+  }, []);
   return (
     <Grid
       id="usecase"
@@ -10,7 +30,7 @@ const UseCase = () => {
         marginBottom: 50,
       }}
       container
-      spacing={2}
+      spacing={4}
     >
       <Grid
         style={{
@@ -23,7 +43,7 @@ const UseCase = () => {
         md={12}
       >
         <Typography align="center" variant="h5" component="h5" gutterBottom>
-          أنشئ سير العمل الذي يناسبك{" "}
+          أنشئ قائمة الطعام الخاصة بك على الإنترنت
         </Typography>
         <Typography
           align="center"
@@ -34,15 +54,66 @@ const UseCase = () => {
             color: "#666",
           }}
         >
-          خصص اللوحات والتطبيقات لتلائم أسلوب عملك
+          خصص قائمة الطعام الخاصة بك واجعلها متاحة على الإنترنت للجميع
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={6} md={6}>
-        image
-      </Grid>
-      <Grid item xs={12} sm={6} md={6}>
-        Description
-      </Grid>
+      {map(UseCases, (use: UseCaseInterface, index: number) => {
+        return index % 2 === 0 ? (
+          <React.Fragment key={index}>
+            <Grid item xs={12} sm={6} md={6}>
+              <img className="use__img" src={use.image} alt={use.title} />
+            </Grid>
+            <Grid
+              style={{
+                marginTop: 50,
+                display: "block",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              item
+              xs={12}
+              sm={6}
+              md={6}
+            >
+              <Typography className="img__text">{use.title}</Typography>
+              <Typography className="img__text__description">
+                {use.description}
+              </Typography>
+              <Button variant="outlined" color="secondary">
+                <Link href={use.button?.link}>{use.button?.title}</Link>
+              </Button>
+            </Grid>
+            <Divider />
+          </React.Fragment>
+        ) : (
+          <React.Fragment key={index}>
+            <Grid
+              style={{
+                marginTop: 50,
+                display: "block",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              item
+              xs={12}
+              sm={6}
+              md={6}
+            >
+              <Typography className="img__text">{use.title}</Typography>
+              <Typography className="img__text__description">
+                {use.description}
+              </Typography>
+              <Button variant="outlined" color="secondary">
+                <Link href={use.button.link}>{use.button.title}</Link>
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <img className="use__img" src={use.image} alt={use.title} />
+            </Grid>
+            <Divider />
+          </React.Fragment>
+        );
+      })}
     </Grid>
   );
 };
